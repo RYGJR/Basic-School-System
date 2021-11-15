21ac8a5872393b707ffd3e53597ebeb27ed33f7c
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-15 17:15:27 +0800

    Added Main Controller Java

--- src/main/java/demo/softdevproject/demo/controller/MainController.java
+++ src/main/java/demo/softdevproject/demo/controller/MainController.java
@@ -1,7 +1,15 @@
 package demo.softdevproject.demo.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
-	
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
 }
