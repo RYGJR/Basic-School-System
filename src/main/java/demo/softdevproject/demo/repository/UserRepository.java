fb97d3b3c7533a1f45ab6b0698d8f3a36993e9a3
LanceManganti <sparthenon19@gmail.com>
2021-11-15 18:26:47 +0800

    Updated UserRep

--- src/main/java/demo/softdevproject/demo/repository/UserRepository.java
+++ src/main/java/demo/softdevproject/demo/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package demo.softdevproject.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import demo.softdevproject.demo.model.User;
 
-public interface UserRepository {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    User findByEmail(String email);
 }
