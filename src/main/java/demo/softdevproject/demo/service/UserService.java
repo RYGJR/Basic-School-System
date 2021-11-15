90d15ce03ae92455a42170b10fda138f3f5c19d5
LineofNile <paguio.jamesd@gmail.com>
2021-11-15 18:06:06 +0800

    UserService(Updated)

--- src/main/java/demo/softdevproject/demo/service/UserService.java
+++ src/main/java/demo/softdevproject/demo/service/UserService.java
@@ -1,7 +1,11 @@
 package demo.softdevproject.demo.service;
 
+import demo.softdevproject.demo.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+import demo.softdevproject.demo.model.User;
 
-public interface UserService {
-
+public interface UserService extends UserDetailsService{
+    User save(UserRegistrationDto registrationDto);
 }
+
