45609965094a95627550503fa2740f4bd15c491e
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-15 17:25:40 +0800

    Added Student service

--- src/main/java/demo/softdevproject/demo/service/StudentService.java
+++ src/main/java/demo/softdevproject/demo/service/StudentService.java
@@ -1,7 +1,14 @@
 package demo.softdevproject.demo.service;
 
+import java.util.List;
 
+import demo.softdevproject.demo.model.Student;
+import org.springframework.data.domain.Page;
 
 public interface StudentService {
-
+    List<Student> getAllStudent();
+    void saveStudent(Student student);
+    Student getStudentById(long id);
+    void deleteStudentById(long id);
+    Page<Student> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
