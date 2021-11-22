78ec86577c8410278d273d35dc8cdc657bde5eed
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-22 20:26:28 +0800

    Added TeacherClass Service

--- src/main/java/demo/softdevproject/demo/service/TeacherClassService.java
+++ src/main/java/demo/softdevproject/demo/service/TeacherClassService.java
@@ -1,4 +1,14 @@
 package demo.softdevproject.demo.service;
 
+import java.util.List;
+
+import demo.softdevproject.demo.model.TeacherClass;
+import org.springframework.data.domain.Page;
+
 public interface TeacherClassService {
+    List<TeacherClass> getAllTeacherClass();
+    void saveTeacherClass(TeacherClass teacherClass);
+    TeacherClass getTeacherClassById(long id);
+    void deleteTeacherClassById(long id);
+    Page<TeacherClass> findPaginated2(int pageNo2, int pageSize2, String sortField2, String sortDirection2);
 }
