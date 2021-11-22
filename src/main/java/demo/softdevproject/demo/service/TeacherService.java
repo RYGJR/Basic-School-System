30f6fbec573ec0de416963b151745c3fa531e55e
LineofNile <paguio.jamesd@gmail.com>
2021-11-22 20:35:26 +0800

    Teacher Service Updated

--- src/main/java/demo/softdevproject/demo/service/TeacherService.java
+++ src/main/java/demo/softdevproject/demo/service/TeacherService.java
@@ -1,4 +1,14 @@
 package demo.softdevproject.demo.service;
 
+import java.util.List;
+
+import demo.softdevproject.demo.model.Teacher;
+import org.springframework.data.domain.Page;
+
 public interface TeacherService {
+    List<Teacher> getAllTeacher();
+    void saveTeacher(Teacher teacher);
+    Teacher getTeacherById(long id);
+    void deleteTeacherById(long id);
+    Page<Teacher> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
