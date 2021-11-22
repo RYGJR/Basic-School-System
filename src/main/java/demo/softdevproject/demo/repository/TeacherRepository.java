1152c2a7b57976d89ac7b569870be059230d5e48
LineofNile <paguio.jamesd@gmail.com>
2021-11-22 20:35:05 +0800

    Teacher Repository Updated

--- src/main/java/demo/softdevproject/demo/repository/TeacherRepository.java
+++ src/main/java/demo/softdevproject/demo/repository/TeacherRepository.java
@@ -1,4 +1,10 @@
 package demo.softdevproject.demo.repository;
 
-public interface TeacherRepository {
+import demo.softdevproject.demo.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long>{
+
 }
