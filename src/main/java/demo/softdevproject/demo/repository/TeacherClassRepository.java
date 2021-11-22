6374186d48d23308e193439b987d825457f4bf57
LanceManganti <sparthenon19@gmail.com>
2021-11-22 20:56:19 +0800

    Added TeacherClassRepo

--- src/main/java/demo/softdevproject/demo/repository/TeacherClassRepository.java
+++ src/main/java/demo/softdevproject/demo/repository/TeacherClassRepository.java
@@ -1,4 +1,9 @@
 package demo.softdevproject.demo.repository;
 
-public interface TeacherClassRepository {
+import demo.softdevproject.demo.model.TeacherClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherClassRepository extends JpaRepository<TeacherClass, Long> {
 }
